cf92f4c0b57f353e88fc9846eaa9ef6677b39a04
daniel <daniel.lehner030303@gmail.com>
2021-05-27 11:13:30 +0200

    routing yeet

--- serviceFrontend/mainComponent/src/app/app.module.ts
+++ serviceFrontend/mainComponent/src/app/app.module.ts
@@ -1,25 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { Router, RouterModule } from "@angular/router";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceComponentComponent } from './service-component/service-component.component';
 import { EmployeesListComponent } from './employees-list/employees-list.component';
 
 import { DataTablesModule } from "angular-datatables";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ServiceComponentComponent,
-    EmployeesListComponent
-  ],
   imports: [
+    DataTablesModule,
     BrowserModule,
-    AppRoutingModule,
-    DataTablesModule
+    RouterModule.forRoot([
+      {path: 'employee-list', component: EmployeesListComponent},
+      {path: 'service-list', component: ServiceComponentComponent}
+    ])
   ],
   providers: [],
+  declarations: [AppComponent, EmployeesListComponent, ServiceComponentComponent],
+  exports: [RouterModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+export class EmployeesListModule { }
+export class ServiceComponentModule { }
