f3760e0a1264f480789ef84072e7132c48c4cf6b
daniel <daniel.lehner030303@gmail.com>
2021-06-09 15:13:54 +0200

    fixed backend

--- backend/src/main/java/at/htlgkr/dto2/Dto2Application.java
+++ backend/src/main/java/at/htlgkr/dto2/Dto2Application.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Dto2Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Dto2Application.class, args);
-    }
+    public static void main(String[] args) { SpringApplication.run(Dto2Application.class, args);}
 
 }
