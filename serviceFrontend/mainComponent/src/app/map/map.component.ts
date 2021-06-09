ab4761f586d649673a98743605f7cfd76beca27e
Jan Führer <jan_fue@icloud.com>
2021-06-09 21:51:10 +0200

    map stuff

--- serviceFrontend/mainComponent/src/app/map/map.component.ts
+++ serviceFrontend/mainComponent/src/app/map/map.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { AgmCoreModule } from "@agm/core";
+import {ChangeDetectorRef, Component, Input, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-map',
@@ -7,12 +7,15 @@ import { AgmCoreModule } from "@agm/core";
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
-  latitude: number = 47;
-  longitude: number = 26;
+  latitude: number = 0;
+  longitude: number = 0;
 
-  constructor() { }
+  constructor(private route : ActivatedRoute, private cdRef:ChangeDetectorRef) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.latitude = Number(params['lat']);
+      this.longitude = Number(params['long']);
+    });
   }
-
 }
