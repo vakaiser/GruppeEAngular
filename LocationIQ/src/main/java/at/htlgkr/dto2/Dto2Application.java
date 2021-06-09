bbf24716c2a47c615d4c19c8ce231ceb1747508b
daniel <daniel.lehner030303@gmail.com>
2021-06-09 15:14:38 +0200

    LocationIQ forward and reverse Geocoding

---
+++ LocationIQ/src/main/java/at/htlgkr/dto2/Dto2Application.java
@@ -0,0 +1,19 @@
+package at.htlgkr.dto2;
+
+import at.htlgkr.dto2.LocationIQ.LocationIQParser;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dto2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dto2Application.class, args);
+        LocationIQParser lamo = new LocationIQParser();
+        String lonlat = lamo.getLonAndLat("Empire State Building");
+        System.out.println(lonlat);
+        String address = lamo.getAdress(lonlat.split(",")[0], lonlat.split(",")[1]);
+        System.out.println(address);
+    }
+
+}
---
+++ LocationIQ/src/main/java/at/htlgkr/dto2/LocationIQ/LocationIQParser.java
@@ -0,0 +1,44 @@
+package at.htlgkr.dto2.LocationIQ;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+
+public class LocationIQParser {
+    private RestTemplate restTemplate;
+    private static final String accessToken = "c2185716076e66";
+
+    public LocationIQParser() {
+        this.restTemplate = new RestTemplate();
+    }
+
+    public String getAdress(String lon, String lat)
+    {
+        String address =restTemplate.getForObject("https://eu1.locationiq.com/v1/reverse.php?key="+ accessToken +"&lat="+ lat +"&lon="+ lon +"&format=json", String.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            JsonNode jsonNode = objectMapper.readTree(address);
+            address = jsonNode.get("display_name").asText();
+            return address;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+       return null;
+    }
+
+    public String getLonAndLat(String address)
+    {
+        String location =restTemplate.getForObject("https://eu1.locationiq.com/v1/search.php?key="+ accessToken +"&q="+ address +"&format=json", String.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            JsonNode jsonNode = objectMapper.readTree(location);
+            location = jsonNode.get(0).get("lon").asText() + "," + jsonNode.get(0).get("lat").asText();
+            return location;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
